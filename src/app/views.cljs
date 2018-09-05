ea75f5407cadd6be99ece82b3db2e66c66a65dbc
Peter Wen <peter@peterwen.com>
2018-09-05 16:34:11 -0700

    cleanup

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -70,24 +70,6 @@
                           :lead "Ben Wen"
                           :accounting "xero"
                           :status "Ready to File"
-                          :action  "File"}
-              :client-05 {:id :client-05
-                          :name "Newaya, LLC"
-                          :return-id "2016:S:S454754160:V2"
-                          :desc "Real clients"
-                          :form "1120S"
-                          :lead "Peter Wen"
-                          :accounting "qbo"
-                          :status "Ready For Review"
-                          :action  "Review"}
-              :client-06 {:id :client-06
-                          :name "PartnershipTest"
-                          :return-id "2016:S:S434564160:V2"
-                          :desc "101 Main St"
-                          :form "1065"
-                          :lead "Ben Wen"
-                          :accounting "xero"
-                          :status "Ready to File"
                           :action  "File"}})
 (rf/reg-sub
  :clients
@@ -135,13 +117,8 @@
                              :grey)
                     :text-decoration :none}}
         "♥"]
-       [:a {:on-click (fn [e]
-                        (.preventDefault e)
-                        (if active-client?
-                          (rf/dispatch [:unactive-client id])
-                          (rf/dispatch [:active-client id])))
-            :href "#"}
-           (:name client)]]))
+
+      " "(:name client)]))
 
 
 (defn clients-panel
@@ -160,22 +137,10 @@
      [:h4 ":active-client state"]
      (pr-str @(rf/subscribe [:active-client]))]]])
 
-
-(defn active-client-component [id client]
- (let [active-client? @(rf/subscribe [:active-client? id])]
-   [:div
-    [:div (if active-client?
-            [:div.ui.card [:div.content [:div.header (:name client)]
-                           [:div.meta (:id client)]
-                           [:div.description (:desc client)]]
-              [:br]]
-      ;[:div [sa/Card {:image {:src "https://source.unsplash.com/random/100x100"} :centered true :header (:name client) :description (:desc client) :meta (:form client) }]]
-           [:a])]]))
-
 (defn app []
   [:div
    [header]
-   [:div [clients-panel]]
+   [clients-panel]
    [counter]])
 
 (rf/dispatch [:initialize-clients])
